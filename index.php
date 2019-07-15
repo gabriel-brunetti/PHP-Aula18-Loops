76f0bb0d3786d5d1db6319f4c08d2ba3d0ece82e
gabriel-brunetti <gabriel.brunetti1@gmail.com>
2019-07-15 09:03:06 -0300

    questão 2 pronta

--- index.php
+++ index.php
@@ -14,7 +14,19 @@
                 <?php 
                     for ($i = 1; $i <= 100; $i++)
                     {
-                        echo $i . " , ";
+                        echo ($i . " , ");
+                    }
+                ?>
+            </div>
+        </li>
+        <li>
+            <p>Modificar o exercício anterior para que, em vez de parar no número 100, pare
+em um número gerado aleatoriamente entre 0 e 100</p>
+            <div>
+                <?php
+                    for ($i=0; $i <= mt_rand(0,100) ; $i++) 
+                    { 
+                        echo ($i . " , ");
                     }
                 ?>
             </div>
