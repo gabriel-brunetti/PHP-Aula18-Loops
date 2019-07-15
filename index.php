3e3379aebb0fcad5e8e09d2be11e7e27c54e7b79
gabriel-brunetti <gabriel.brunetti1@gmail.com>
2019-07-15 08:51:01 -0300

    Tarefa 1, ok!

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +7,18 @@
     <title>Aula 18 - Exercícios</title>
 </head>
 <body>
-    
+    <ol>
+        <li>
+            <p>Utilizando um for, imprimir os números de 1 a 100</p>
+            <div>
+                <?php 
+                    for ($i = 1; $i <= 100; $i++)
+                    {
+                        echo $i . " , ";
+                    }
+                ?>
+            </div>
+        </li>
+    </ol>
 </body>
 </html>
